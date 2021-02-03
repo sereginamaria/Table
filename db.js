db58dc62ce58c41537f5a667c7bd82a8d32e548d
Maria <60785083+sereginamaria@users.noreply.github.com>
2021-02-03 15:35:57 +0300

    update files

--- db.js
+++ db.js
@@ -5,98 +5,112 @@ export let data = {
       "name": "Иван",
       "points_earned": 862,
       "points_spend": 325,
-      "registration_date": "2014-01-01"
+      "registration_date": "2021-01-04"
     },
     {
       "id": "5c41crgrwgd56",
       "name": "Георг",
       "points_earned": 862,
       "points_spend": 4556,
-      "registration_date": "2014-01-01"
+      "registration_date": "2021-01-06"
     },
     {
       "id": "5c41c56drd56",
       "name": "Урсула",
       "points_earned": 862,
       "points_spend": 543,
-      "registration_date": "2014-01-01"
+      "registration_date": "2021-01-15"
     },
     {
       "id": "5c41c56c4656",
       "name": "Енот",
       "points_earned": 862,
       "points_spend": 166,
-      "registration_date": "2014-01-01"
+      "registration_date": "2021-01-16"
     },
     {
       "id": "5c41c56cd456",
       "name": "Паскаль",
       "points_earned": 862,
       "points_spend": 347,
-      "registration_date": "2014-01-01"
+      "registration_date": "2021-01-18"
     },
     {
       "id": "5c41c56cd59",
       "name": "Корталь",
       "points_earned": 862,
       "points_spend": 9876,
-      "registration_date": "2014-01-01"
+      "registration_date": "2021-01-29"
     },
     {
       "id": "5c41c56cd59",
       "name": "Егор",
       "points_earned": 862,
       "points_spend": 123,
-      "registration_date": "2014-01-01"
+      "registration_date": "2021-01-30"
     },
     {
       "id": "5c41c56cd59",
       "name": "Игорь",
       "points_earned": 862,
       "points_spend": 236,
-      "registration_date": "2014-01-01"
+      "registration_date": "2021-01-02"
     },
     {
       "id": "5c41c56cd59",
       "name": "Ярослав",
       "points_earned": 862,
       "points_spend": 463,
-      "registration_date": "2014-01-01"
+      "registration_date": "2021-01-07"
     },
     {
       "id": "5c41c56cd59",
       "name": "Даниил",
       "points_earned": 862,
       "points_spend": 835,
-      "registration_date": "2014-01-01"
+      "registration_date": "2021-01-17"
     },
     {
       "id": "5c41c56cd59",
       "name": "Даниил",
       "points_earned": 862,
       "points_spend": 835,
-      "registration_date": "2014-01-01"
+      "registration_date": "2021-01-10"
     },
     {
       "id": "5c41c56cd59",
       "name": "Даниил",
       "points_earned": 862,
       "points_spend": 835,
-      "registration_date": "2014-01-01"
+      "registration_date": "2021-01-25"
     },
     {
       "id": "5c41c56cd59",
       "name": "Даниил",
       "points_earned": 862,
       "points_spend": 835,
-      "registration_date": "2014-01-01"
+      "registration_date": "2021-01-28"
     },
     {
       "id": "5c41c56cd59",
       "name": "Даниил",
       "points_earned": 862,
       "points_spend": 835,
-      "registration_date": "2014-01-01"
+      "registration_date": "2021-01-21"
+    },
+    {
+      "id": "5c41c56cd59",
+      "name": "Даниил",
+      "points_earned": 862,
+      "points_spend": 835,
+      "registration_date": "2021-01-21"
+    },
+    {
+      "id": "5c41c56cd59",
+      "name": "Даниил",
+      "points_earned": 862,
+      "points_spend": 835,
+      "registration_date": "2021-01-21"
     }
   ]
 }
--- src/components/vTabel.vue
+++ src/components/vTabel.vue
@@ -42,23 +42,54 @@
                                         ></b-form-select>
                                 </b-form-group>
                         </b-col>
+
+                        <b-col lg="6" class="my-1">
+                                <b-form-group
+                                        label="Filter"
+                                        label-for="filter-input"
+                                        label-cols-sm="3"
+                                        label-align-sm="right"
+                                        label-size="sm"
+                                        class="mb-0"
+                                >
+                                        <b-input-group size="sm">
+                                                <b-form-input
+                                                        id="filter-input"
+                                                        v-model="filter"
+                                                        type="search"
+                                                        placeholder="Type to Search"
+                                                ></b-form-input>
+
+                                                <b-input-group-append>
+                                                        <b-button :disabled="!filter" @click="filter = ''">Clear</b-button>
+                                                </b-input-group-append>
+                                        </b-input-group>
+                                </b-form-group>
+                        </b-col>
+
+<!--                        <b-col sm="5" md="6" class="my-1">
+                                <date-range-picker/>
+                        </b-col>-->
                 </b-row>
                 <b-table
                         striped hover
                         :items="users_data"
                         :fields="fields"
+                        :filter="filter"
                         :current-page="currentPage"
                         :per-page="perPage"
                         :sort-by.sync="sortBy"
                         stacked="md"
                         show-empty
-                        small>
+                        small
+                        @filtered="onFiltered">
                 </b-table>
 
                 <b-col sm="7" md="6" class="my-1">
                         <b-pagination
                                 v-model="currentPage"
                                 :total-rows="totalRows"
+                                v-if="onFiltered"
                                 :per-page="perPage"
                                 align="fill"
                                 size="sm"
@@ -69,6 +100,11 @@
 </template>
 
 <script>
+        import Vue from 'vue'
+        import DateRangePicker from 'vue-mj-daterangepicker'
+        import 'vue-mj-daterangepicker/dist/vue-mj-daterangepicker.css'
+        Vue.use(DateRangePicker)
+
     export default {
             name: "vTabel",
             components: {},
@@ -81,11 +117,14 @@
                                     {key: 'points_spend', label: 'points_spend', sortable: true},
                                     {key: 'registration_date', label: 'registration_date', sortable: true}
                             ],
-
                             currentPage: 1,
                             perPage: 5,
                             pageOptions: [5, 10, 15, {value: 100, text: "Show a lot"}],
                             sortBy: '',
+                            filter: null,
+                            filterOn: [],
+                            row: 1
+
                     }
             },
             props: {
@@ -101,9 +140,137 @@
                                             return { text: f.label, value: f.key }
                                     })
                     },
-                    totalRows(){
-                            return this.users_data.length
-                    }
+                    totalRows: {
+                            get: function () {
+                                    if(this.row === 1){
+                                            return this.users_data.length
+                                    } else {
+                                            return this.row
+                                    }
+                            },
+                            set: function (newRow) {
+                                    this.row = newRow
+                            }
+                    },
             },
+            methods: {
+                    onFiltered(filteredItems) {
+                            this.totalRows = filteredItems.length
+                    },
+            }
     }
 </script>
+
+
+<!--<template>
+        <b-container fluid>
+                &lt;!&ndash; User Interface controls &ndash;&gt;
+                <b-row>
+                        <b-col lg="6" class="my-1">
+                                <b-form-group
+                                        label="Filter"
+                                        label-for="filter-input"
+                                        label-cols-sm="3"
+                                        label-align-sm="right"
+                                        label-size="sm"
+                                        class="mb-0"
+                                >
+                                        <b-input-group size="sm">
+                                                <b-form-input
+                                                        id="filter-input"
+                                                        v-model="filter"
+                                                        type="search"
+                                                        placeholder="Type to Search"
+                                                ></b-form-input>
+
+                                                <b-input-group-append>
+                                                        <b-button :disabled="!filter" @click="filter = ''">Clear</b-button>
+                                                </b-input-group-append>
+                                        </b-input-group>
+                                </b-form-group>
+                        </b-col>
+
+                        <b-col sm="7" md="6" class="my-1">
+                                <b-pagination
+                                        v-model="currentPage"
+                                        :total-rows="totalRows"
+                                        :per-page="perPage"
+                                ></b-pagination>
+                        </b-col>
+                </b-row>
+
+                &lt;!&ndash; Main table element &ndash;&gt;
+                <b-table
+                        :items="items"
+                        :fields="fields"
+                        :current-page="currentPage"
+                        :per-page="perPage"
+                        :filter="filter"
+                        @filtered="onFiltered">
+                </b-table>
+
+        </b-container>
+</template>
+
+<script>
+        export default {
+                data() {
+                        return {
+                                items: [
+                                        { isActive: true, age: 40, name: { first: 'Dickerson', last: 'Macdonald' } },
+                                        { isActive: false, age: 21, name: { first: 'Larsen', last: 'Shaw' } },
+                                        {
+                                                isActive: false,
+                                                age: 9,
+                                                name: { first: 'Mini', last: 'Navarro' },
+                                                _rowVariant: 'success'
+                                        },
+                                        { isActive: false, age: 89, name: { first: 'Geneva', last: 'Wilson' } },
+                                        { isActive: true, age: 38, name: { first: 'Jami', last: 'Carney' } },
+                                        { isActive: false, age: 27, name: { first: 'Essie', last: 'Dunlap' } },
+                                        { isActive: true, age: 40, name: { first: 'Thor', last: 'Macdonald' } },
+                                        {
+                                                isActive: true,
+                                                age: 87,
+                                                name: { first: 'Larsen', last: 'Shaw' },
+                                                _cellVariants: { age: 'danger', isActive: 'warning' }
+                                        },
+                                        { isActive: false, age: 26, name: { first: 'Mitzi', last: 'Navarro' } },
+                                        { isActive: false, age: 22, name: { first: 'Genevieve', last: 'Wilson' } },
+                                        { isActive: true, age: 38, name: { first: 'John', last: 'Carney' } },
+                                        { isActive: false, age: 29, name: { first: 'Dick', last: 'Dunlap' } }
+                                ],
+                                fields: [
+                                        { key: 'name', label: 'Person full name', sortable: true, sortDirection: 'desc' },
+                                        { key: 'age', label: 'Person age', sortable: true, class: 'text-center' },
+                                        {
+                                                key: 'isActive',
+                                                label: 'Is Active',
+                                                formatter: (value) => {
+                                                        return value ? 'Yes' : 'No'
+                                                },
+                                                sortable: true,
+                                                sortByFormatted: true,
+                                                filterByFormatted: true
+                                        },
+                                        { key: 'actions', label: 'Actions' }
+                                ],
+                                totalRows: 1,
+                                currentPage: 1,
+                                perPage: 5,
+                                filter: null,
+                        }
+                },
+                mounted() {
+                        // Set the initial number of items
+                        this.totalRows = this.items.length
+                },
+                methods: {
+                        onFiltered(filteredItems) {
+                                // Trigger pagination to update the number of buttons/pages due to filtering
+                                this.totalRows = filteredItems.length
+                                this.currentPage = 1
+                        }
+                }
+        }
+</script>-->
